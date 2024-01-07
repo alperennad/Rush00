c7aa472190823ed93df5a7e23dbbbeda726b601d
alperennad <146393353+alperennad@users.noreply.github.com>
2024-01-07 21:14:33 +0300

    alperennad

---
+++ main.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademirca <ademirca@student.42.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 04:37:39 by ibcelik           #+#    #+#             */
+/*   Updated: 2024/01/07 20:35:41 by ademirca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	rush(int width, int height);
+
+int	ft_atoi(char *c)
+{
+	int	res;
+	int	sign;
+	int	i;
+
+	i = 0;
+	sign = 1;
+	while (c[i] == 32 || (c[i] >= 9 && c[i] <= 13))
+		i++;
+	if (c[i] == '-' || c[i] == '+')
+	{
+		if (c[i] == '-')
+			sign = -1;
+		i++;
+	}
+	res = 0;
+	while (c[i] >= '0' && c[i] <= '9')
+	{
+		res = res * 10 + (c[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 3)
+	{
+		rush(ft_atoi(av[1]), ft_atoi(av[2]));
+		return (0);
+	}
+}
---
+++ rush01.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush01.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ademirca <ademirca@student.42.com.tr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 05:03:37 by ibcelik           #+#    #+#             */
+/*   Updated: 2024/01/07 20:35:20 by ademirca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	x_times(int a, char b)
+{
+	while (a > 0)
+	{
+		ft_putchar(b);
+		a--;
+	}
+}
+
+void	first(int length)
+{
+	while (length > 0)
+	{
+		ft_putchar('/');
+		break ;
+	}
+	if (length == 2)
+		ft_putchar(92);
+	else if (length >= 3)
+	{
+		x_times(length - 2, '*');
+		ft_putchar(92);
+	}
+	ft_putchar('\n');
+}
+
+void	middle(int length)
+{
+	ft_putchar('*');
+	if (length == 2)
+		ft_putchar('*');
+	else if (length >= 3)
+	{
+		x_times(length - 2, ' ');
+		ft_putchar('*');
+	}
+	ft_putchar('\n');
+}
+
+void	last(int length)
+{
+	ft_putchar(92);
+	if (length == 2)
+		ft_putchar('/');
+	else if (length >= 3)
+	{
+		x_times(length - 2, '*');
+		ft_putchar('/');
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int width, int height)
+{
+	int	k;
+
+	if ((width <= 0 || height < 0) || (width < 0 || height <= 0))
+		write(1, "ERROR!\n", 7);
+	else
+	{
+		first(width);
+		k = 2;
+		while (k < height)
+		{
+			middle(width);
+			k++;
+		}
+		if (height > 1)
+		{
+			last(width);
+		}
+	}
+}
